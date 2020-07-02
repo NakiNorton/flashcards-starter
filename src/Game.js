2bfd64f0c5a880e83a896aeb6128c96062b621c2
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-02 16:40:07 -0600

    Refactor game class

--- src/Game.js
+++ src/Game.js
@@ -11,14 +11,14 @@ class Game {
   }
 
   start() {
-    const cards = prototypeQuestions.map(quizQuestion => {
-      const card = new Card(quizQuestion.id, quizQuestion.question, quizQuestion.answers, quizQuestion.correctAnswer)
+    const cards = prototypeQuestions.map(flashCard => {
+      const card = new Card(flashCard.id, flashCard.question, flashCard.answers, flashCard.correctAnswer)
       return card;
     });
     const deck = new Deck(cards);
     const round = new Round(deck);
     this.currentRound = round;
-    this.printMessage(deck, this.currentRound);
+    this.printMessage(deck); 
     this.printQuestion(this.currentRound);
   }
 
@@ -33,22 +33,3 @@ class Game {
 }
 
 module.exports = Game;
-
-
-// Your Game class should meet these other requirements:
-// Should keep track of the currentRound
-/////// updateRound method? currentRound++ after Round is instantiated?
-// start: method that starts everything
-////// Creates Cards
-///// Puts Cards in a Deck
-////// Creates a new Round using the Deck
-////// invokes printMessage to display the message in the CLI
-///// invokes printQuestion to kick off our helper functions that allow interaction via the CLI
-
-
-
-
-// VM969: 4 Card { id: 1, question: "What allows you to define a set of related information using key-value pairs?", answers: Array(3), correctAnswer: "object" }
-// VM969: 4 Card { id: 2, question: "What is a comma-separated list of related values?", answers: Array(3), correctAnswer: "array" }
-// VM969: 4 Card { id: 3, question: "What type of prototype method directly modifies the existing array?", answers: Array(3), correctAnswer: "mutator method" }
-// VM969: 4 Card { id: 4, question: "What type of prototype method does not modify the …returns a particular representation of the array?", answers: Array(3), correctAnswer: "accessor method" }
