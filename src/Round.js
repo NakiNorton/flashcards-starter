9dd72075cb3367ce614d5e1c2037dafb90cdb643
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-02 16:39:17 -0600

    Refactor round class

--- src/Round.js
+++ src/Round.js
@@ -4,8 +4,8 @@ class Round {
   constructor(deck) {
     this.deck = deck;
     this.currentCard = deck.cards[0];
-    this.index = 0;
     this.turns = 0;
+    this.index = 0;
     this.incorrectGuesses = [];
   }
 
@@ -17,23 +17,23 @@ class Round {
     const turn = new Turn(guess, this.currentCard);
     this.turns++;  
     this.index++; 
-    this.currentCard = this.deck.cards[this.index] 
+    this.currentCard = this.deck.cards[this.index]; 
     if (!turn.evaluateGuess(guess)) { 
       this.incorrectGuesses.push(this.currentCard.id);
     }
-    turn.giveFeedback(guess);
+    return turn.giveFeedback(guess);
   }
 
   calculatePercentCorrect() { 
     const correctAnswers = this.turns - this.incorrectGuesses.length;
-    const percentCorrect = (correctAnswers / this.turns) * 100;
-    return percentCorrect + '%';
+    const percentCorrect = Math.floor(correctAnswers / this.turns * 100);
+    return percentCorrect;
   }
 
   endRound() {
     console.log(`** Round over! ** You answered 
-    ${this.calculatePercentCorrect()} of the questions correctly!`);
-    // return process.exit();
+    ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    return process.exit();
   }   
 }
 
