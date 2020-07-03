7c9e46af5ab6e7dd37567e8e4e1a9c71765d1801
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-02 19:17:59 -0600

    Refactor tests to make more specific

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,54 +7,51 @@ const Card = require('../src/Card');
 describe ('Turn', function() {
 
   let card;
-  beforeEach('flash card deck', function () {
+  let turn;
+  beforeEach('flash card deck', function() {
     card = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    turn = new Turn('array', card);
   });
 
-  it('should be a function', function () {
+  it('should be a function', function() {
     expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function () {
-    const turn = new Turn();
+  it('should be an instance of Turn', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
+  it('should have all keys', function () {
+    expect(turn).to.have.all.keys('guess', 'card');
+  });
+
   it('should have a guess', function() { 
-    const turn = new Turn('array', card); 
     expect(typeof(turn.guess)).to.equal('string');
-    // make test specific to one of the possible answers???
   }); 
     
   it('should have a card', function() {
-    const turn = new Turn('array', card);
-    expect(typeof(turn.card)).to.equal('object'); 
-    expect(turn.card).to.equal(card);
+    expect(turn.card).to.be.a.instanceof(Card);
   }); 
 
   it('should return the guess', function() {
-    const turn = new Turn('array', card);
     expect(turn.returnGuess()).to.equal('array');
   });
 
   it('should return the card', function() {
-    const turn = new Turn('array', card);
     expect(turn.returnCard()).to.equal(card);
   });
 
-  it('should return a boolean value reflecting if guess is correct', function() {
-    const turn = new Turn('array', card);
+  it('should return a boolean reflecting if guess is correct', function() {
     expect(turn.evaluateGuess()).to.equal(true);
-    const turn1 = new Turn('object', card);
-    expect(turn1.evaluateGuess()).to.equal(false);
+    const turn2 = new Turn('function', card);
+    expect(turn2.evaluateGuess()).to.equal(false);
   });
 
   it('should return feedback based on the guess', function() {
-    const turn = new Turn('array', card);
     turn.evaluateGuess();
     expect(turn.giveFeedback()).to.equal('correct!');
-    const turn1 = new Turn('object', card);
-    turn1.evaluateGuess();
-    expect(turn1.giveFeedback()).to.equal('incorrect!');
+    const turn2 = new Turn('function', card)
+    turn2.evaluateGuess();
+    expect(turn2.giveFeedback()).to.equal('incorrect!');
   });
 });
