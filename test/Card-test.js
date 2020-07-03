f95848b50cb89b5e9c278de058bc7cc663df3d07
Stephanie Norton <steph.jane.norton@gmail.com>
2020-07-02 19:19:30 -0600

    Refactor tests to make more specific

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,36 +1,40 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const Card = require('../src/Card');
 
 describe('Card', function() {
+
+  let card;
+  beforeEach(function () {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
 
   it('should be a function', function() {
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', function() {
-    const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   }); 
 
-  it('should have an id', function () {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  it('should have all keys', function() {
+    expect(card).to.have.all.keys('id', 'question', 'answers', 'correctAnswer');
+  });
+
+  it('should have a number for an id', function() {
     expect(card.id).to.equal(1);
   });  
 
   it('should have a question', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
   it('should have a list of possible answers', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    expect(card.answers).to.be.an('array').with.lengthOf(3)
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
   it('should have the correct answer', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     expect(card.correctAnswer).to.equal('object');
   });
 });
